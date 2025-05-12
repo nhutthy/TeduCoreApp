e387254c0f88c5288ee4eac61c2118c08e42056e
Nhut Thy <maytinhnhutthy@gmail.com>
2025-05-12 21:58:22 +0700

    B12

---
+++ TeduCoreApp.Data/Entities/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TeduCoreApp.Data.Enums;
+using TeduCoreApp.Data.Interfaces;
+
+namespace TeduCoreApp.Data.Entities
+{
+    [Table("AppUsers")]
+    public class AppUser : IdentityUser<Guid>, IDateTracking, ISwitchable
+    {
+        public string FullName { get; set; }
+
+        public DateTime? BirthDay { set; get; }
+
+        public decimal Balance { get; set; }
+
+        public string Avatar { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ TeduCoreApp.Data/Entities/Language.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using TeduCoreApp.Data.Enums;
+using TeduCoreApp.Data.Interfaces;
+using TeduCoreApp.Infrastructure.SharedKernel;
+
+namespace TeduCoreApp.Data.Entities
+{
+    [Table("Languages")]
+    public class Language : DomainEntity<string>, ISwitchable
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public string Resources { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
